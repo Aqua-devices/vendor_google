82d93d03ec93a790b07f6135c26e8a86b8c82e3d
Giuseppe Maggio <jertlok@protonmail.com>
2019-03-23 11:17:14 +0100

    Dreamliner: Add Pixel Stand application
    Change-Id: I1d53dbd82caec5a68be38f2646c38384bb8c3687

--- crosshatch/Android.mk
+++ crosshatch/Android.mk
@@ -345,6 +345,8 @@ LOCAL_SRC_FILES := proprietary/priv-app/DreamlinerPrebuilt/DreamlinerPrebuilt.ap
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
